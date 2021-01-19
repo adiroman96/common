5fd3f47d7b2cfcca5a2a213d0e69bf2cba991aa7
Adrian Roman <58778585+adiroman96@users.noreply.github.com>
2021-01-19 14:21:43 +0200

    Update

--- build/middlewares/error-handler.js
+++ build/middlewares/error-handler.js
@@ -4,7 +4,6 @@ exports.errorHandler = void 0;
 var custom_error_1 = require("../errors/custom-error");
 var errorHandler = function (err, req, res, next) {
     if (err instanceof custom_error_1.CustomError) {
-        console.log(err);
         return res.status(err.statusCode).send({ errors: err.serializeErrors() });
     }
     res.status(400).send({
